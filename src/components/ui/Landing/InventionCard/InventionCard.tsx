351ae10c722dbe56428854341b35636b96da41a6
hex01e <hex01e@student.1337.ma>
2025-05-20 08:36:08 +0100

    chore: documented InventorCard

--- src/components/ui/Landing/InventionCard/InventionCard.tsx
+++ src/components/ui/Landing/InventionCard/InventionCard.tsx
@@ -3,16 +3,16 @@ import './InventionCard.css'
 import blueArrow from './images/arrow.svg'
 
 interface InventionCardProps {
-	title: string;
-	description: string;
-	image: string;
+	title: string; // Invention title
+	image: string; // Invention image
+	description: string; // invention description -could be change later-
 }
 
 function InventionCard({ title, description, image }: InventionCardProps) {
 	return (
 		<div className="InventionCard">
-			<img src={image} alt="" className='InventionImg'/>
-			<img src={blueArrow} alt="" className="blueArrow" />
+			<img src={image} alt="InventionImage" className='InventionImg'/>
+			<img src={blueArrow} alt="blueArrow" className="blueArrow" />
 			<div className="InventionCardContent">
 				<h2>{title}</h2>
 				<p>{description}</p>
