3d3ffa014e11b138ed235c1c0671b97d133c64d8
hex01e <houmanso@student.1337.ma>
2025-05-23 09:14:54 +0100

    feat: add support for additional image formats in custom type definitions

--- src/types/custom.d.ts
+++ src/types/custom.d.ts
@@ -13,3 +13,13 @@ declare module '*.jpg' {
     const content: string;
     export default content;
 }
+
+declare module '*.jpeg' {
+    const content: string;
+    export default content;
+}
+
+declare module '*.webp' {
+    const content: string;
+    export default content;
+}
