19789f45293b63b59324aca8ada576dbf7f0147e
hex01e <houmanso@student.1337.ma>
2025-05-30 17:28:28 +0100

    feat: added Innovation Diagnosis quiz

--- src/router.tsx
+++ src/router.tsx
@@ -6,7 +6,7 @@ import Incubator from '@pages/landing/incubator/Incubator';
 import TechCenter from '@pages/landing/tech_center/tech_center';
 import FAQ from '@pages/landing/faq/FAQ';
 import NotFound from '@pages/NotFound/NotFound';
-import AuditInnovationForm from '@pages/landing/AuditInnovation/AuditInnovation';
+import AuditInnovationForm from '@pages/landing/AuditInnovation/InnovationDiagnosis';
 
 const router = createBrowserRouter([
   {
