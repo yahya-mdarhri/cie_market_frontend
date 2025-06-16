f20163afa38f629f21a2639cd9633e2ca5325dd8
hex01e <houmanso@student.1337.ma>
2025-06-16 08:27:42 +0100

    feat: create data module for partners, stats, and departments; refactor Home component to utilize new data structure

--- src/pages/landing/home/home.tsx
+++ src/pages/landing/home/home.tsx
@@ -1,78 +1,17 @@
-import LandingLayout, { HeroProps, StatsItemsType } from "@layouts/LandingLayout/LandingLayout";
+import LandingLayout from "@layouts/LandingLayout/LandingLayout";
 import "./home.css";
 import DepartmentHeadSection from "@ui/Landing/DepartmentHeadSection/DepartmentHeadSection";
 
 import arrow from '../../../assets/icons/arrow.svg';
-
 import headImg from "./images/M-Bouya.png";
-
-import cdg from './images/partners/cdg.jpg';
-import cloudflare from './images/partners/cloudflare.jpeg';
-import dxc from './images/partners/dxc.webp';
-import safari from './images/partners/SAFARI.jpg';
-import yanaHolding from './images/partners/Ynna-holding.png';
-import eu from './images/partners/European_Union.webp';
-import efficacity from './images/partners/Efficacity.png';
 
 import Introduction from "@ui/Landing/Introduction/Introduction";
 import VisionAndMission from "@ui/Landing/VisionAndMission/VisionAndMission";
 import Partners from "@ui/Landing/Partners/Partners";
 import { useDocumentTitle } from "@hooks/useDocumentTitle";
 import { Link } from 'react-router-dom';
-import { FaHandshake, FaIndustry, FaLightbulb, FaTools } from 'react-icons/fa';
 import ContactUs from "@ui/Landing/ContactUs/ContactUs";
-
-const partners = [
-		{
-			logo: cdg,
-			name: "CDG",
-			link: "https://www.cdg.ma/"
-		},
-		{
-			logo: cloudflare,
-			name: "Cloudflare",
-			link: "https://www.cloudflare.com/"
-		},
-		{
-			logo: dxc,
-			name: "DXC",
-			link: "https://dxc.com/"
-		},
-		{
-			logo: safari,
-			name: "SAFARI",
-			link: "https://www.groupe-safari.com/"
-		},
-		{
-			logo: yanaHolding,
-			name: "Yana Holding",
-			link: "https://www.ynna.ma/"
-		},
-		{
-			name: "European Union",
-			logo: eu,
-			link: "https://european-union.europa.eu/"
-		},
-		{
-			name: "Efficacity",
-			logo: efficacity,
-			link: "https://efficacity.com/",
-		}
-]
-
-const statsData:StatsItemsType = {
-	a: { name: "Innovation & Patents", value: 644 },
-	b: { name: "International Patents", value: 117 },
-	c: { name: "Gold Medals", value: 2 },
-};
-
-const heroProps: HeroProps = {
-	heroTitle: <>From Idea to Market Bridging Innovation and Industry</>,
-	heroDescription: "We are glad to have you here. Explore our features and services.",
-	firstAction: "Partner With Us",
-	secondAction: "Submit A Patent",
-	stats: statsData,
-};
+import { departments, heroProps, partners } from './data';
 
 interface CIEItemProps {
 	name: string;
@@ -96,33 +35,6 @@ function CIEItem({name, description, icon, link}: CIEItemProps) {
 		</Link>
 	);
 }
-
-const departments = [
-    {
-        icon: <FaHandshake size={40} color="var(--primary-color)" />,
-        name: "Technology Transfer Office",
-        description: "Bridging Innovation and Industry through Research Commercialization",
-        link: "/tto"
-    },
-    {
-        icon: <FaIndustry size={40} color="var(--primary-color)" />,
-        name: "Industrial Clinic",
-        description: "Solving Real-World Challenges Through Academic–Industry Collaboration",
-        link: "/industrial"
-    },
-    {
-        icon: <FaLightbulb size={40} color="var(--primary-color)" />,
-        name: "Incubator",
-        description: "Empowering Startups to Transform Ideas into Impact",
-        link: "/incubator"
-    },
-    {
-        icon: <FaTools size={40} color="var(--primary-color)" />,
-        name: "Tech Center",
-        description: "Driving Innovation with Cutting-Edge Tools, Talent, and Infrastructure",
-        link: "/tech-center"
-    }
-];
 
 function Home() {
 	useDocumentTitle('Home | Centre for Innovation and Entrepreneurship');
