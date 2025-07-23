cab72179df2057ac52f305622081e3bb40e9343a
yahya mdarhri <78668874+yahya-mdarhri@users.noreply.github.com>
2025-07-23 17:09:34 +0100

    23072025

--- src/pages/landing/home/data.tsx
+++ src/pages/landing/home/data.tsx
@@ -55,9 +55,9 @@ export const partners = [
 // this array contains the stats data for the hero section
 export const getStatsData = (t: any): StatsItemsType => {
     return {
-        a: { name: t('stats.innovationPatents'), value: 644 },
-        b: { name: t('stats.internationalPatents'), value: 117 },
-        c: { name: t('stats.goldMedals'), value: 2 },
+        a: { name: t('stats.innovationPatents'), value: 681 },
+        b: { name: t('stats.internationalPatents'), value: 119 },
+        c: { name: t('stats.medals'), value: 7 },
     };
 }
 
